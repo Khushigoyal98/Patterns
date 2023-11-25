61ee0537ddb8aa3471fad2020f06e6b954da46b9
Khushigoyal98 <98528290+Khushigoyal98@users.noreply.github.com>
2023-11-25 15:40:37 +0530

    Add files via upload

---
+++ pattern1.cpp
@@ -0,0 +1,25 @@
+/*
+Draw the following pattern using only 2 loops
+* * * * *
+* 2 3 4 *
+* 2 3 4 *
+* 2 3 4 *
+* * * * *
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	for(int i=1;i<=5;i++){
+		for(int j=1;j<=5;j++){
+			if(i==1||i==5||j==1||j==5)
+			cout<<"*";
+			else{
+				cout<<j;
+			}
+		
+		}
+		cout<<endl;
+	}
+}
