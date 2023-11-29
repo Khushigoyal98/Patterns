debf0228d83bf77324c8fe225384f8b2f4007e8e
Khushi Goyal <khushigoyal1104@gmail.com>
2023-11-29 16:06:14 +0530

    added pattern 5

---
+++ pattern5.cpp
@@ -0,0 +1,22 @@
+/*
+0 2 3 4 5
+1 0 3 4 5
+1 2 0 4 5
+1 2 3 0 5
+1 2 3 4 0
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+	for(int i=1;i<=5;i++){
+		for(int j=1;j<=5;j++){
+			if(i==j){
+				cout<<0<<" ";
+			}else{
+				cout<<j<<" ";
+			}
+		}
+		cout<<endl;
+	}
+}
