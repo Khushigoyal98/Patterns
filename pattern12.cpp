7ae71eda518b7cd7b8262f1796314dd3351dd962
Khushi Goyal <khushigoyal1104@gmail.com>
2023-12-02 19:31:22 +0530

    pattern11, pattern12, pattern13 added

---
+++ pattern12.cpp
@@ -0,0 +1,21 @@
+/*
+ j 1 2 3 4 5
+i 
+5  1 2 3 4 5
+4  1 2 3 4
+3  1 2 3
+2  1 2
+1  1
+
+*/
+
+#include<iostream>
+using namespace std;
+ int main(){
+ 	int i,j;
+ 	for(i=5;i>=1;i--){
+ 		for(j=1;j<=i;j++){
+ 			cout<<j<<" ";
+		 }cout<<endl;
+	 }
+ }
---
+++ pattern13.cpp
@@ -0,0 +1,22 @@
+/*
+ j 1 2 3 4 5
+i
+1  1 2 3 4 5
+2  2 3 4 5
+3  3 4 5
+4  4 5
+5  5
+
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+	int i,j;
+	for(i=5;i>=1;i--){
+		int temp=6-i;
+		for(j=1;j<=i;j++){
+			cout<<temp++<<" ";
+		}cout<<endl;
+	}
+}
