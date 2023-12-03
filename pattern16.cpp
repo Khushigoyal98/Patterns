d3a690f7e0647c3715d831ef1d1400c01d5a56d4
Khushi Goyal <khushigoyal1104@gmail.com>
2023-12-03 17:23:15 +0530

    pattern16 added

---
+++ pattern16.cpp
@@ -0,0 +1,22 @@
+/*
+ j 1 2 3 4 5
+i
+i  5 4 3 2 1
+2  5 4 3 2
+3  5 4 3
+4  5 4
+5  5
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int i,j;
+	for(i=5;i>=1;i--){
+		for(j=1;j<=i;j++){
+			cout<<6-j<<" ";
+		}cout<<endl;
+	}
+}
