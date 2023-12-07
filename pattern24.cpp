99b1e12e7c789c39300e562a37d66538a490e313
Khushi Goyal <khushigoyal1104@gmail.com>
2023-12-07 22:30:57 +0530

    pattern24 added

---
+++ pattern24.cpp
@@ -0,0 +1,24 @@
+/*
+ j 1 2 3 4 
+i
+1  0
+2  1 2 
+3  3 4 0
+4  1 2 3 4
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int i,j;
+	int x=0;
+	for(i=1;i<=4;i++){
+		for(j=1;j<=i;j++){
+			cout<<x++<<" ";
+			if(x==5){
+				x=0;
+			}
+		}cout<<endl;
+	}
+}
