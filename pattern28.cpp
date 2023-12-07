91882b6c0c8334ff55bfeafbae9ddedf683fca81
Khushi Goyal <khushigoyal1104@gmail.com>
2023-12-07 23:13:36 +0530

    pattern28 added

---
+++ pattern28.cpp
@@ -0,0 +1,25 @@
+/* 
+ j 1 2 3 4 5 6
+i 
+1  @ @ @ @ @ !
+2  @ @ @ @ ! @
+3  @ @ @ ! @ @
+4  @ @ ! @ @ @ 
+5  @ ! @ @ @ @
+6  ! ! ! ! ! ! 
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+	int i,j;
+	for(i=1;i<=6;i++){
+		for(j=1;j<=6;j++){
+			if(i+j==7 || i==6){
+				cout<<"!"<<" ";
+			}else{
+				cout<<"@ ";
+			}
+		}cout<<endl;
+	}
+}
