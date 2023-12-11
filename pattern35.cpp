3319277e1a4e726f56c16f48dd8c5a4b8e2b011e
Khushi Goyal <khushigoyal1104@gmail.com>
2023-12-11 11:13:26 +0530

    pattern35 added

---
+++ pattern35.cpp
@@ -0,0 +1,22 @@
+/* 
+1 2 3 4 5
+A A A A A
+1 2 3 4 5
+A A A A A
+1 2 3 4 5
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+	int i,j;
+	for(i=1;i<=5;i++){
+		for(j=1;j<=5;j++){
+			if(i%2==0){
+				cout<<"A ";
+			}else{
+				cout<<j<<" ";
+			}
+		}cout<<endl;
+	}
+}
