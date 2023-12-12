bd3fee37214bf09222589d4215061f3b89246b35
Khushi Goyal <khushigoyal1104@gmail.com>
2023-12-12 23:08:49 +0530

    pattern37 added

---
+++ pattern37.cpp
@@ -0,0 +1,26 @@
+/*
+ij 1 2 3 4 5 6 7
+   7 7 7 7 7 7 7
+1             7
+2            7
+3		    7
+4	  	   7
+5	      7
+6	     7
+7       7
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+	int i,j;
+	for(i=1;i<=7;i++){
+		for(j=1;j<=7;j++){
+			if(i==1||i+j==8){
+				cout<<"7 ";
+			}else{
+				cout<<"  ";
+			}
+		}cout<<endl;
+	}
+}	      
