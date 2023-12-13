cda04d12240736b5ea670068468d9fd30c7d2821
Khushigoyal98 <98528290+Khushigoyal98@users.noreply.github.com>
2023-12-13 12:53:26 +0530

    Add files via upload

---
+++ pattern41.cpp
@@ -0,0 +1,25 @@
+/*
+ j 1 2 3 4 5 6 7 8 9
+i          
+1          1
+2        1 2 3
+3      1 2 3 4 5
+4    1 2 3 4 5 6 7 
+5  1 2 3 4 5 6 7 8 9
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int i,j,k;
+	int arr[]={1,3,5,7,9};
+	for(i=1;i<=5;i++){
+		for(k=5;k>=i;k--){
+		cout<<" ";
+			}
+			for(j=1;j<=arr[i-1];j++){
+				cout<<j;
+			}cout<<endl;
+	}
+}
