caeafaad634d5ddadb8ce3331ec397564fcf7b46
Khushi Goyal <khushigoyal1104@gmail.com>
2023-12-15 02:37:37 +0530

    pattern44 added

---
+++ pattern44.cpp
@@ -0,0 +1,23 @@
+/* 
+1 2 3 4 5
+* * * * *
+1 2 3 4 5
+* * * * *
+1 2 3 4 5
+*/
+
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main(){
+	int i,j;
+	for(i=1;i<=5;i++){
+		for(j=1;j<=5;j++){
+			if(i%2==0){
+				cout<<"* ";
+			}else{
+				cout<<j<<" ";
+			}
+		}cout<<endl;
+	}
+}
