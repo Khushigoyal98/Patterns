b041043e91fdcfe07c3c6b380dfddda976c901f5
Khushigoyal98 <98528290+Khushigoyal98@users.noreply.github.com>
2023-12-17 13:17:02 +0530

    pattern48 added

---
+++ pattern48.cpp
@@ -0,0 +1,16 @@
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	int i,j;
+	for(i=1;i<=11;i++){
+		for(j=1;j<=11;j++){
+			if(i==1||j==1||i==11||j==11||i==j||i+j==12||i+j==7||j-i==5||i-j==5||i+j==17){
+				cout<<"*  ";
+			}else{
+				cout<<"   ";
+			}
+		}cout<<endl;
+	}
+}
