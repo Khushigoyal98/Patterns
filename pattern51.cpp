27369aeeb8ada327b9b84ae1fb24dbdada44ee79
Khushigoyal98 <98528290+Khushigoyal98@users.noreply.github.com>
2023-12-20 20:01:40 +0530

    Add files via upload

---
+++ pattern51.cpp
@@ -0,0 +1,40 @@
+/*
+
+Draw following pattern using  loops
+
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+
+
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 10; i++)
+    {
+        for (j = 1; j <= 5; j++)
+        {
+            if (i == 5 || (i + j >= 6 && i < 5) || (i + j <= 11 && i > 5))
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+	return 0;
+}
