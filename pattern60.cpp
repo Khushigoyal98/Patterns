7e769d978004e1a72fd0c627ce3c39f2ac2ed574
Khushigoyal98 <98528290+Khushigoyal98@users.noreply.github.com>
2023-12-27 21:22:56 +0530

    pattern 60 added

---
+++ pattern60.cpp
@@ -0,0 +1,26 @@
+/*
+ j 1 2 3 4 5 6 7
+i
+1        *
+2      * * *
+3    * * * * *
+4  * * * * * * *
+5    * * * * *
+6      * * * 
+7        *
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+	int i,j;
+	for(i=1;i<=7;i++){
+		for(j=1;j<=7;j++){
+			if((i<=4 && i+j>=5 && j-i<=3)||(i>4 && i-j<=3 && i+j<=11)){
+				cout<<"* ";
+			}else{
+				cout<<"  ";
+			}
+		}cout<<endl;
+	}
+}
