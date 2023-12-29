742522e4291f30e6dddbee01841942749282a2d5
Khushigoyal98 <98528290+Khushigoyal98@users.noreply.github.com>
2023-12-29 22:27:46 +0530

    new pattern added

---
+++ pattern61.cpp
@@ -0,0 +1,46 @@
+
+
+
+/*
+
+Draw following pattern using only two loops
+
+      * * * *
+    * * * * *
+  * *     * *
+* *       * *
+          * *
+          * *
+          * *
+          * *
+  * * * * * * * * * *
+
+*/
+
+
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 8; i++)
+    {
+        for (j = 1; j <= 7; j++)
+        {
+            if (j == 6 || j == 7 || (i <= 4 && i + j == 5) || (i <= 4 && i + j == 6) || (i == 2 && j == 5))
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+	cout<<"  * * * * * * * * * *";
+	return 0;
+}
